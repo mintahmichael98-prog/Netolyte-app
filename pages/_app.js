25e9c685990922cc6b36f7f8bab6ee62fd3a7eb8
mintahmichael98-prog <mintahmichael98@gmail.com>
2025-12-29 15:51:08 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,11 @@ import { ClerkProvider } from '@clerk/nextjs';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  // Check if key exists to prevent crashing if env is missing
+  const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
   return (
-    // Explicitly pass the key here
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={publishableKey}>
       <Component {...pageProps} />
     </ClerkProvider>
   );
