66459f636cef0342e96ce98872ceb86a7aa5547b
mintahmichael98-prog <mintahmichael98@gmail.com>
2025-12-29 15:33:34 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-import App from '../App';
+import dynamic from 'next/dynamic';
+
+// This tells Next.js to load the App component ONLY on the client side
+const AppWithNoSSR = dynamic(() => import('../App'), {
+  ssr: false,
+});
 
 export default function Home() {
-  return <App />;
+  return <AppWithNoSSR />;
 }
