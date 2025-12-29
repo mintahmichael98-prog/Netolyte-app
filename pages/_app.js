c45f1e61da765d9d1282bfef2a116720556f9bff
mintahmichael98-prog <mintahmichael98@gmail.com>
2025-12-29 18:57:28 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,11 +7,13 @@ import '../styles/globals.css';
 export default function MyApp({ Component, pageProps }) {
   const [mounted, setMounted] = useState(false);
 
+  // Prevents "Hydration Mismatch" errors in React 19/Next 15
   useEffect(() => {
     setMounted(true);
   }, []);
 
   if (!mounted) {
+    // A simple dark background to show while the app loads
     return <div style={{ background: '#020617', minHeight: '100vh' }} />;
   }
 
