2e2e5f03e4a612fc9d285da7f90fd52be358b45c
mintahmichael98-prog <mintahmichael98@gmail.com>
2025-12-29 17:37:54 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import dynamic from 'next/dynamic';
-
-// This tells Next.js to load the App component ONLY on the client side
-const AppWithNoSSR = dynamic(() => import('../App'), {
-  ssr: false,
-});
+import React from 'react';
+// Import any components you need from your components folder here
+// import Header from '../components/Header'; 
 
 export default function Home() {
-  return <AppWithNoSSR />;
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Welcome to Netolyte</h1>
+      <p>Your Next.js 15 application is now running correctly.</p>
+      
+      {/* If you had logic in App.js that you need here, 
+         you can paste the JSX for your main dashboard/home UI below.
+      */}
+      
+      <div className="card">
+        <p>Edit <code>pages/index.js</code> to get started.</p>
+      </div>
+    </main>
+  );
 }
